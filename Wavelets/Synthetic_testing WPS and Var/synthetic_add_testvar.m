49e0cd2d90e58c63bf6c62a956593cc5899937b7
rpalermo <rpalermo@mit.edu>
2018-11-12 11:10:41 -0500

    commenting

--- Wavelets/Synthetic_testing WPS and Var/synthetic_add_testvar.m
+++ Wavelets/Synthetic_testing WPS and Var/synthetic_add_testvar.m
@@ -1,11 +1,23 @@
-% test synthetic
+% THIS CODE TESTS SYNTHETIC SIGNALS 
+
+% first signal is a sine wave with amplitude 4
+% second signal is a higher frequency sine wave
+% third signal is the sum of the two
+
+% first "band" added is eq14_1 -- corresponds to higher frequency band
+% second "band" added is eq14_2 -- lower frequency band
+% sum of the two
+
+% run over each signal to see if variance of individual signals
+% recovered in sum of two signals
+%%
 
 save_on = false;
 n=2; i = 13;
 
 
 
-% ADD SINE WAVES
+% SIGNAL 1
 deltad = pi/50;
 t=0:deltad:50*pi;
 T = t(end)+deltad;
@@ -14,6 +26,7 @@ figure()
 plot(t,4*sin(t))
 dowave_sat(synth,deltad,n,t,synth,[],save_on,[],i);
 
+% SIGNAL 2
 deltad = pi/50;
 t=0:deltad:50*pi;
 T = t(end)+deltad;
@@ -22,6 +35,7 @@ figure()
 plot(t,sin(t/pi/2))
 dowave_sat(synth,deltad,n,t,synth,[],save_on,[],i);
 
+% SIGNAL 3
 deltad = pi/50;
 t=0:deltad:50*pi;
 T = t(end)+deltad;
