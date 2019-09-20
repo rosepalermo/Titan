cd8006f5e68982e25a332c2e5767b92ffd0c0c40
rpalermo <rpalermo@mit.edu>
2019-09-20 19:48:19 -0400

    engaging compatibility things

---
+++ Tadpole 2/CoupledTadpoleCoast/mex_cdmnoslr.m
@@ -0,0 +1,3 @@
+mex -setup
+mex -v -g in_environment.cpp visilibity.o
+mex -v -g visibility_polygon.cpp visilibity.o
--- Tadpole 2/CoupledTadpoleCoast/setup_cdmnoslr.sh
+++ Tadpole 2/CoupledTadpoleCoast/setup_cdmnoslr.sh
@@ -1,5 +1,4 @@
 make clean
 make main
-mex -setup
-mex -v -g in_environment.cpp visilibity.o
-mex -v -g visibility_polygon.cpp visilibity.o
+module load mit/matlab/2018a
+module add gcc/6.3.0
