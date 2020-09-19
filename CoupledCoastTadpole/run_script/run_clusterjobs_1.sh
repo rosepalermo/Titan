999bad13c1ebb8294552102457ba8c944a97bd81
rpalermo <rpalermo@mit.edu>
2020-09-18 23:21:40 -0400

    run all

--- CoupledCoastTadpole/run_script/run_clusterjobs_1.sh
+++ CoupledCoastTadpole/run_script/run_clusterjobs_1.sh
@@ -1,13 +1,13 @@
 #!/bin/bash  
 # run all my code
-# sbatch rand_uniform_runs1.slurm
-# sbatch rand_wave_runs1.slurm
-# sbatch rand_wave_runs2.slurm
-# sbatch rand_wave_runs3.slurm
-# sbatch river_uniform_runs1.slurm
-# sbatch river_wave_runs1.slurm
-# sbatch river_wave_runs2.slurm
-# sbatch river_wave_runs3.slurm
+sbatch rand_uniform_runs1.slurm
+sbatch rand_wave_runs1.slurm
+sbatch rand_wave_runs2.slurm
+sbatch rand_wave_runs3.slurm
+sbatch river_uniform_runs1.slurm
+sbatch river_wave_runs1.slurm
+sbatch river_wave_runs2.slurm
+sbatch river_wave_runs3.slurm
 sbatch rand_wave_uniform_run1.slurm
 sbatch rand_wave_uniform_run1.slurm
 sbatch rand_wave_uniform_run1.slurm
