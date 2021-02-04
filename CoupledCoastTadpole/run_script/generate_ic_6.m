a249b530edbb67410c6b7a9eafa5756135647522
rpalermo <rpalermo@mit.edu>
2021-02-04 14:41:13 -0500

    generate_ic.sh

---
+++ CoupledCoastTadpole/run_script/generate_ic_6.m
@@ -0,0 +1,37 @@
+cd ..
+
+folder = fileparts(which('getpath_CCT.m'));
+addpath(genpath(folder));
+
+rfactor = 0.25; % 0.25; % depth of the depression as a function of relief of the noise surface
+init_temp = cell(0);
+ii = 1;
+for idx = 6001:7000
+load('p_init_v1.mat')
+p.dtmax = 1e6;
+p.Kf = 5e-6;
+p.rand_gen = 1;
+p.size_final = 2.4;
+p.saveint = 40;
+% p.sealevel_init = 20;
+[init_temp,p] = get_IC(p,rfactor,idx);
+
+solution = Tadpole(init_temp,p);
+
+init{idx} = solution(:,:,end);
+
+lake = init{idx}<40;
+
+% does the SL hit a boundary?
+[p] = check_boundary(lake,p);
+
+% if it doesn't hit the boundary, then we're good
+if ~isfield(p,'boundary')
+   idx_save(ii) = idx;
+   ii = ii+1;
+end
+clearvars p
+save('/home/rpalermo/TitanModelOutput/generate_init/idx_test_6','idx')
+end
+
+save('/home/rpalermo/TitanModelOutput/generate_init/ic_generated_6','init','idx_save')
---
+++ CoupledCoastTadpole/run_script/run_generate_ic.sh
@@ -0,0 +1,11 @@
+#!/bin/bash  
+# run generate_ic_code
+sbatch generate_ic_1.slurm
+sbatch generate_ic_2.slurm
+sbatch generate_ic_3.slurm
+sbatch generate_ic_4.slurm
+sbatch generate_ic_5.slurm
+sbatch generate_ic_6.slurm
+sbatch generate_ic_7.slurm
+sbatch generate_ic_8.slurm
+sbatch generate_ic_9.slurm
